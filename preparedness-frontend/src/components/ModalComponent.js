fdc90491f88237cda28713d61957af0f3188037d
haliespaige <halie.romasco@gmail.com>
2023-06-02 15:56:52 -0700

    Completed Front End Assessment

--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,16 @@
 import React from "react"
+import { Modal, ModalHeader, ModalBody} from "reactstrap";
 
-const ModalComponent = () => {
-  return <></>
-}
+const ModalComponent = ({ isOpen, toggle, name }) => {
+  return (
+    <Modal isOpen={isOpen} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Hello there {name}! </ModalHeader>
+      <ModalBody>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </ModalBody>
+    </Modal>
+  );
+};
+
 
 export default ModalComponent
